bd3019c7b8cf858779ebb32e5f2117fe9cd9a1be
ivangdzulfikar <ivangahmad@gmail.com>
2023-12-13 12:09:26 +0700

    tambah fungsi count

--- app/Livewire/Click.php
+++ app/Livewire/Click.php
@@ -6,9 +6,21 @@ use Livewire\Component;
 
 class Click extends Component
 {
+    public $count;
+
     public function handlerClick()
     {
         dump("click");
+    }
+
+    public function increment()
+    {
+        $this->count++;
+    }
+
+    public function decrement()
+    {
+        $this->count--;
     }
     
     public function render()
--- resources/views/livewire/click.blade.php
+++ resources/views/livewire/click.blade.php
@@ -1,3 +1,7 @@
 <div>
     <button wire:click="handlerClick">Click</button>
+
+    <p>{{ $count }}</p>
+    <button wire:click="increment">+</button>
+    <button wire:click="decrement">-</button>
 </div>
